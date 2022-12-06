1173650865c4afc620231fee50ebcfc31d94bb18
jennifer-kadri <jennifer.kadri@epitech.eu>
2022-12-06 14:17:59 +0100

    Deploy test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          We're gonna try to deploy this app...
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          It's working !
-        </a>
-      </header>
+      <h1>This is a demo of the react app</h1>
     </div>
   );
 }
